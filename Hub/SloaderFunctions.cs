4dc7c8a8d06950361cef2f4e0372339ae619c876
Robert Muehsig <robert.muehsig@codeinside.eu>
2022-01-23 23:27:49 +0100

    hub backend

---
+++ Hub/SloaderFunctions.cs
@@ -0,0 +1,39 @@
+using System;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace Hub
+{
+    public class SloaderFunctions
+    {
+        [FunctionName("Sloader")]
+        public void Run([TimerTrigger("0 */5 * * * *")] TimerInfo myTimer, ILogger log)
+        {
+            log.LogInformation($"Sloader Function invoked at: {DateTime.Now}");
+
+            log.LogInformation($"SloaderRunner Version: {typeof(Sloader.Engine.SloaderRunner).Assembly.GetName().Version}");
+
+            Sloader.Config.SloaderConfigLocator locator = new Sloader.Config.SloaderConfigLocator();
+            var allConfigs = locator.FromGitHub("Code-Inside", "Hub", "_data", "*.Sloader.yml").GetAwaiter().GetResult();
+
+            foreach (var configUrl in allConfigs)
+            {
+                try
+                {
+                    log.LogInformation($"Sloader run for: {configUrl}");
+                    Sloader.Engine.SloaderRunner.AutoRun(configUrl).GetAwaiter().GetResult();
+                }
+                catch(Exception exc)
+                {
+                    log.LogError("Exception: " + exc.Message);
+                    log.LogError("Exception StackTrace: " + exc.StackTrace);
+                }
+            }
+
+            log.LogInformation($"Sloader done at: {DateTime.Now}");
+
+        }
+    }
+
+}
